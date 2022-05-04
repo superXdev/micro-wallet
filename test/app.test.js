ed86ba89356bb2611f31536b2086c47758d98bae
Fikri <fikrizhanking@gmail.com>
2022-05-04 21:15:03 +0700

    Fix cli test

--- test/app.test.js
+++ test/app.test.js
@@ -1,18 +1,18 @@
 const { expect } = require("chai");
 const fs = require("fs");
 const { execSync } = require("child_process");
+const { runSetup } = require('../cmds/modules/setup');
 
 const runCommand = (args) => {
   return execSync(`node cli.js ${args}`).toString();
 };
 
 describe("CLI", () => {
-  it("should run setup command", () => {
-    expect(runCommand("setup")).to.include("Everything is ready!");
-  });
-
-  after((done) => {
-    fs.unlinkSync('./user.db')
-    done()
+  before((done) => {
+    runSetup().then(() => done())
   })
-});
+
+  it("should run setup command", () => {
+    expect(runCommand("setup")).to.include("Already setting up");
+  })
+})
