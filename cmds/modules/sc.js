58b9d74e770bb802a9b3b4168be849cce5b0cce8
Fikri <fikrizhanking@gmail.com>
2022-05-23 19:00:32 +0700

    Add verify contract functionality

--- cmds/modules/sc.js
+++ cmds/modules/sc.js
@@ -1,5 +1,6 @@
 const fs = require('fs')
 const { getEstimateGasLimit } = require('../../utils/web3')
+const axios = require('axios')
 
 
 async function deployContract(data) {
@@ -8,6 +9,51 @@ async function deployContract(data) {
 }
 
 
+async function verifyContract(data) {
+   const params = new URLSearchParams()
+
+   params.append('apikey', data.apiKey)
+   params.append('module', 'contract')
+   params.append('action', 'verifysourcecode')
+   params.append('sourceCode', data.sourceCode)
+   params.append('contractaddress', data.address)
+   params.append('codeformat', 'solidity-single-file')
+   params.append('contractname', data.contractName)
+   params.append('compilerversion', data.compilerversion)
+   params.append('OptimizationUsed', data.OptimizationUsed)
+
+   const config = {
+      headers: {
+         'Content-Type': 'application/x-www-form-urlencoded'
+      }
+   }
+
+   const result = await axios.post(data.url, params, config)
+
+   return result
+}
+
+
+function getApiUrl(networkId, isTestnet) {
+   const data = {
+      BNB: {
+         mainnet: 'https://api.bscscan.com/api',
+         testnet: 'https://api-testnet.bscscan.com/api'
+      }
+   }
+
+   if(data[networkId] === undefined) {
+      return null
+   }
+
+   const typeNetwork = isTestnet ? 'testnet' : 'mainnet'
+
+   return data[networkId][typeNetwork]
+}
+
+
 module.exports = {
-   deployContract
+   deployContract,
+   verifyContract,
+   getApiUrl
 }
--- cmds/sc_cmds/verify.js
+++ cmds/sc_cmds/verify.js
@@ -1,9 +1,67 @@
 const chalk = require('chalk')
+const fs = require('fs')
+const { verifyContract, getApiUrl } = require('../modules/sc')
+const { getNetworkById } = require('../modules/network')
+
 
 
 exports.command = 'verify'
 exports.desc = 'Verify smart contract source code on explorer'
+exports.builder = {
+  address: {
+  	 demand: true,
+    type: 'string',
+    alias: 'a',
+    desc: 'Contract address'
+  },
+  network: {
+    demand: true,
+    type: 'number',
+    alias: 'n',
+    desc: 'Set network id or identifier'
+  },
+  erc20: {
+    type: 'boolean',
+    desc: 'erc20 type token'
+  },
+}
+
 
 exports.handler = async function (argv) {
+	const networkData = await getNetworkById(argv.network)
 
+	// collection of defacult smart contract parameters
+	const contractDefault = {
+		erc20: {
+			url: getApiUrl(networkData.currencySymbol, networkData.isTestnet),
+			sourceCode: fs.readFileSync('./contracts/ERC20Token.sol').toString(),
+			apiKey: '',
+			address: argv.address,
+			contractName: 'ERC20Token',
+			compilerversion: 'v0.8.14+commit.80d49f37',
+			OptimizationUsed: 0 // 0 false, 1 true
+		}
+	}
+
+	let data = null
+	if(argv.erc20) {
+		// API url is null or not available
+		if(contractDefault.erc20.url === null) {
+			return console.log('API not available for this network')
+		}
+
+		// sign data parameters
+		data = contractDefault.erc20
+	}
+
+	// verifying or submit to explorer API
+	const result = await verifyContract(data)
+
+	// show result if successfull
+	if(result.data.message === 'OK') {
+		console.log(chalk.green('Successfully submitted, it will processed between 30 seconds'))
+	} else {
+		// show error message
+		console.log(`Failed with message: ${chalk.red.bold(result.data.result)}`)
+	}
 }
