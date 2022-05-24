8a2b40a5a188c848ba5a0269e924967c9133bd98
Fikri <fikrizhanking@gmail.com>
2022-05-24 19:42:29 +0700

    Refactor wallet list command

--- cmds/wallet_cmds/list.js
+++ cmds/wallet_cmds/list.js
@@ -6,23 +6,26 @@ const chalk = require('chalk')
 exports.command = 'list'
 exports.desc = 'Show all of wallet'
 
-exports.handler = function (argv) {
+exports.handler = async function (argv) {
+   // create table instances
    const walletTable = new Table({
       head: [chalk.white.bold('Wallet name'), chalk.white.bold('Address'), chalk.white.bold('Created at')],
       colWidths: [18, 45, 17]
    });
 
-   getWalletList().then((data) => {
-      data.forEach(row =>  {
-         const date = new Date(row.createdAt)
-         walletTable.push([
-            row.walletName,
-            row.address,
-            date.toLocaleString('en-US', { day: 'numeric', month: 'long', year: 'numeric'})
-         ])
-      })
+   // get wallet list
+   const lists = await getWalletList()
 
-      console.log(walletTable.toString())
+   // re-format and push to table
+   lists.forEach(row =>  {
+      const date = new Date(row.createdAt)
+      walletTable.push([
+         row.walletName,
+         row.address,
+         date.toLocaleString('en-US', { day: 'numeric', month: 'long', year: 'numeric'})
+      ])
    })
 
+   // show the table
+   console.log(walletTable.toString())
 }
