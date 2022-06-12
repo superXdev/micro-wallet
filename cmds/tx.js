2c88b30ecd8919896e2ca81168aae34a62214d14
Fikri <fikrizhanking@gmail.com>
2022-06-12 14:47:25 +0700

    Update tx command to received latest or ID tx

--- cmds/tx.js
+++ cmds/tx.js
@@ -5,6 +5,7 @@ const Web3 = require('web3')
 const TimeAgo = require('javascript-time-ago')
 const en = require('javascript-time-ago/locale/en.json')
 const Listr = require('listr')
+const { History } = require('../utils/database')
 
 
 async function getRevertReason(txHash, rpcURL){
@@ -18,25 +19,53 @@ async function getRevertReason(txHash, rpcURL){
 }
 
 
-exports.command = 'tx <hash>'
-exports.desc = 'Show transaction details by tx hash'
+exports.command = 'tx <tx>'
+exports.desc = 'Show transaction details'
 exports.builder = (yargs) => {
-   yargs.positional('hash', {
-      describe: '64 characters length & random HEX',
+   yargs.positional('tx', {
+      describe: 'Tx hash, latest transaction or ID',
       type: 'string'
    })
    .option('network', {
-      demand: true,
       alias: 'n',
       type: 'string',
       desc: 'Set network id or identifier'
    })
+   .example([
+      ['$0 tx 0x97055fc421020dfebe2ebf734bb84cb466852f393e478dc8d886d0e64d1128fe'],
+      ['$0 tx latest'],
+      ['$0 tx 1']
+   ])
 } 
 
 
 exports.handler = async function (argv) {
+   let opts = {
+      network: argv.network,
+      hash: argv.tx
+   }
+
+   // for latest tx
+   if(argv.tx === 'latest') {
+      const tx = await History.findAll({ 
+         limit: 1,
+         order: [ ['createdAt', 'DESC'] ]
+      })
+
+      opts.network = tx[0].networkId
+      opts.hash = tx[0].hash
+   }
+
+   // if with tx ID
+   if(opts.network === undefined) {
+      const tx = await History.findByPk(argv.tx)
+
+      opts.network = tx.networkId
+      opts.hash = tx.hash
+   }
+
    // get network data
-   const network = await getNetworkById(argv.network)
+   const network = await getNetworkById(opts.network)
    // web3js instances
    const web3 = new Web3(network.rpcURL)
 
@@ -56,7 +85,7 @@ exports.handler = async function (argv) {
       title: 'Fetching data from blockchain',
       task: async () => {
          // get transaction information
-         result = await web3.eth.getTransaction(argv.hash)
+         result = await web3.eth.getTransaction(opts.hash)
 
          // if transaction not found
          if(result === null) {
@@ -71,7 +100,7 @@ exports.handler = async function (argv) {
             time = '-'
          } else {
             // success transaction
-            const result2 = await web3.eth.getTransactionReceipt(argv.hash)
+            const result2 = await web3.eth.getTransactionReceipt(opts.hash)
             const block = await web3.eth.getBlock(result.blockNumber)
             gasPercentUsed = parseFloat(result2.gasUsed / result.gas * 100).toFixed(2)
             const date = new Date(block.timestamp * 1000).toLocaleString()
@@ -83,7 +112,7 @@ exports.handler = async function (argv) {
 
          
          if(isFailed) {
-            reason = await getRevertReason(argv.hash, network.rpcURL)
+            reason = await getRevertReason(opts.hash, network.rpcURL)
          }
       }
    }]).run()
