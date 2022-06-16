e7872b91d41361b0e5a5d0485037a9d5b573441c
Fikri <fikrizhanking@gmail.com>
2022-06-16 10:19:36 +0700

    Add example to add book command

--- cmds/book_cmds/add.js
+++ cmds/book_cmds/add.js
@@ -4,28 +4,30 @@ const { saveAddress } = require('../modules/book')
 
 exports.command = 'add'
 exports.desc = 'Add new address'
-exports.builder = {
-  address: {
-    demand: true,
-    type: 'string',
-    alias: 'a',
-    desc: 'An wallet address'
-  },
-  name: {
-    demand: true,
-    type: 'string',
-    alias: 'n',
-    desc: 'Name or identifier'
-  },
+exports.builder = (yargs) => {
+	yargs.option('address', {
+		demand: true,
+		type: 'string',
+		alias: 'a',
+		desc: 'An wallet address'
+	})
+	.option('name', {
+		demand: true,
+		type: 'string',
+		alias: 'n',
+		desc: 'Name or identifier'
+	})
+	.example([
+		['$0 book add -a 0x00000000000 -n myWallet']
+	])
 }
 
 exports.handler = async function (argv) {
-   const result = await saveAddress(argv.name, argv.address)
+	const result = await saveAddress(argv.name, argv.address)
 
-   if(!result) {
-      return console.log(`Address is not valid 
-or name not alpha numeric characters`)
-   }
+	if(!result) {
+		return console.log(`Address is not valid or name not alpha numeric characters`)
+	}
 
-   console.log(chalk.green('Address successfully saved'))
+	console.log(chalk.green('Address successfully saved'))
 }
